e28ffc33bf87f9d966da9f2e7b459e9aa08b6c68
arthurgao697 <arthurgao697@revature.net>
2022-07-15 08:37:53 -0700

    Implemented read file in __INPUT__ directory

--- 07072022_Project00/__TOOLS__/DictionarySeperator/Program.cs
+++ 07072022_Project00/__TOOLS__/DictionarySeperator/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace DictionarySeperator
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string INPUT_file = @"./__INPUT__/words_alpha.txt";
+            
+            using StreamReader INPUT_fileStream = new StreamReader(INPUT_file);
+            {
+                string WORK_fsLine;
+                while ((WORK_fsLine = INPUT_fileStream.ReadLine()) != null)
+                {
+                    Console.WriteLine(WORK_fsLine.Length + " - " + WORK_fsLine);
+                }
+            }
+        }
+    }
+}
