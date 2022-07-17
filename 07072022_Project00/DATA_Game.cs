3b30735ff64aad6febaf37e2b799446c309310c4
arthurgao697 <arthurgao697@revature.net>
2022-07-17 01:02:39 -0700

    Implemented method to output a random word from database

--- 07072022_Project00/DATA_Game.cs
+++ 07072022_Project00/DATA_Game.cs
@@ -7,7 +7,8 @@ namespace Project_00
     {
         // Fields
         private readonly string DB_PROP_connectionString;
-        private static readonly string DB_COMMAND_validInput = "SELECT TOP 1 (ID) FROM Project00_Wordle.WordDB_Classic WHERE Word = @INPUT_WORD;";
+        private static readonly string DB_COMMAND_validInput = @"SELECT TOP 1 (ID) FROM Project00_Wordle.WordDB_Classic WHERE Word = @INPUT_WORD;";
+        private static readonly string DB_COMMAND_randWord = @"SELECT TOP 1 (Word) FROM Project00_Wordle.WordDB_Classic ORDER BY NEWID();";
         // Constructor
         public DATA_Game()
         {
@@ -39,5 +40,20 @@ namespace Project_00
             }
         }
 
+        public string DATA_Game_randWord()
+        {
+            string OUTPUT_word = "";
+            using SqlConnection DB_connection = new SqlConnection(this.DB_PROP_connectionString);
+            DB_connection.Open();
+            
+            using SqlCommand DB_generateRandWord = new SqlCommand(DB_COMMAND_randWord, DB_connection);
+            using SqlDataReader DB_reader = DB_generateRandWord.ExecuteReader();
+
+            if (DB_reader.Read())
+            {
+                OUTPUT_word = DB_reader.GetString(0);
+            }
+            return OUTPUT_word;
+        }
     }
 }
